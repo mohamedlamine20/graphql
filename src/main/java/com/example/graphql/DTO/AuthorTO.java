68b3d6db689fbd7a2e1bf9776727d798d2dce885
mohamed <mlamine642@gmail.com>
2023-03-29 19:56:57 +0100

    add GitHub action

--- src/main/java/com/example/graphql/DTO/AuthorTO.java
+++ src/main/java/com/example/graphql/DTO/AuthorTO.java
@@ -6,11 +6,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class AuthorTO {
+
+
+    @NotBlank
     private Integer id;
     private String firstName;
     private String lastName;
--- src/main/java/com/example/graphql/GraphQlApplication.java
+++ src/main/java/com/example/graphql/GraphQlApplication.java
@@ -2,12 +2,20 @@ package com.example.graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@CrossOrigin(origins = "http://localhost:8080/graphql")
-public class GraphQlApplication {
 
+@CrossOrigin(origins = "http://localhost:8080/graphql")
+@Controller
+public class GraphQlApplication {
+  @GetMapping("/")
+  public String home(){
+      return "dist/comps/index.html";
+  }
     public static void main(String[] args) {
         SpringApplication.run(GraphQlApplication.class, args);
     }
